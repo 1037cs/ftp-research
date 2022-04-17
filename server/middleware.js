e5939c2ffab2bdfb7b36e1472d508a897af1b4ca
Andrey <1037cs@gmail.com>
2022-04-17 20:42:56 +0400

    removed middleware.js

--- server/middleware.js
+++
@@ -1,8 +0,0 @@
-function setAccessHeaders(req,res,next){
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
-
-	next()
-}
-
-module.exports = setAccessHeaders()
